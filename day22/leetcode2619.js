8d02c665a2e1702fddac6f164244f0acf9802b17
Sanchit Bajaj <sanchit02@outlook.com>
2024-11-27 22:00:42 +0530

    day 22 task complete

---
+++ day22/leetcode2619.js
@@ -0,0 +1,17 @@
+// 2619. Array Prototype Last
+// URL -> https://leetcode.com/problems/array-prototype-last
+
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function () {
+  if (this.length < 1) {
+    return -1;
+  }
+  return this[this.length - 1]
+};
+
+/**
+* const arr = [1, 2, 3];
+* arr.last(); // 3
+*/
