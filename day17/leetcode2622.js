c4718c1c46e2bc43f6cca9abd16336e00711cb24
Sanchit Bajaj <sanchit02@outlook.com>
2024-11-27 21:50:46 +0530

    day 17 task complete

---
+++ day17/leetcode2622.js
@@ -0,0 +1,50 @@
+// 2622. Cache With Time Limit
+// URL -> https://leetcode.com/problems/cache-with-time-limit/
+
+
+var TimeLimitedCache = function () {
+  // this.cache = {}
+  // this.count = 0;
+  this.cache = new Map()
+};
+
+/** 
+* @param {number} key
+* @param {number} value
+* @param {number} duration time until expiration in ms
+* @return {boolean} if un-expired key already existed
+*/
+TimeLimitedCache.prototype.set = function (key, value, duration) {
+  const alreadyExists = this.cache.get(key)
+
+  if (alreadyExists) {
+    clearTimeout(alreadyExists.timeoutId);
+  }
+  const timeoutId = setTimeout(() => {
+    this.cache.delete(key);
+  }, duration)
+  this.cache.set(key, { value, timeoutId });
+  return Boolean(alreadyExists);
+};
+
+/** 
+* @param {number} key
+* @return {number} value associated with key
+*/
+TimeLimitedCache.prototype.get = function (key) {
+  return this.cache.has(key) ? this.cache.get(key).value : -1;
+};
+
+/** 
+* @return {number} count of non-expired keys
+*/
+TimeLimitedCache.prototype.count = function () {
+  return this.cache.size;
+};
+
+/**
+* const timeLimitedCache = new TimeLimitedCache()
+* timeLimitedCache.set(1, 42, 1000); // false
+* timeLimitedCache.get(1) // 42
+* timeLimitedCache.count() // 1
+*/
